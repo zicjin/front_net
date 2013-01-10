d103f2469e963aaa1897283cdba22b9f83df6f9e
Cheney Jin <zicjin@gmail.com>
2013-01-10 23:34:12 +0800

    update gallery

--- gallery/jquery-color/transport.js
+++
@@ -1,9 +0,0 @@
-/**
- * @package https://raw.github.com/jquery/jquery-color/master/package.json
- * @src https://raw.github.com/jquery/jquery-color/{{version}}/jquery.color.js
- */
-
-define('#{{id}}', ['${{debug}}'], function(require, exports, module) {
-  var jQuery = require('${{debug}}');
-  /*{{code}}*/
-});
