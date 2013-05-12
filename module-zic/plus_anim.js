d70744ee7c9b4be687f8ef7c996aedfd9ce38f73
zicjin <zicjin@gmail.com>
2013-05-13 01:23:49 +0800

    .

--- module-zic/plus_anim.js
+++ module-zic/plus_anim.js
@@ -7,11 +7,11 @@ define(function (require, exports, module) {
             itemClass: "red"
         }, options);
 
-        this.on("click.pluanim", function () {
+        this.on("click.plusanim", function () {
             var _this = $(this);
             if (_this.children("i").hasClass(opts.statusClass)) return;
             var r = _this.offset();
-            var i = $('<div>', { class: opts.itemClass, style: 'font-size:10px;z-index:1000' });
+            var i = $('<div>', { class: opts.itemClass, style: 'font-size:10px;z-index:1000', text: '+1' });
             i.appendTo("body");
             r.top += 7;
             r.left += 30;
