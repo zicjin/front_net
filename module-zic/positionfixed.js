620ceffd302c27cc43796ae27af901ed8f7d3f7f
cheney jin <zicjin@gmail.com>
2014-03-21 15:49:33 +0800

    .

--- module-zic/positionfixed.js
+++ module-zic/positionfixed.js
@@ -9,20 +9,25 @@ define(function (require, exports, module) {
     $.fn.positionfixed = function (options) {
         var opts = $.extend({
             margin: 0,
-            lazy: 0, //normal 300
-            callBack: false
+            lazy: 10, //eye critical point
+            callBack: false,
+            minWidth: 0
         }, options)
 
         _this = $(this)
         _this.css({
-            'width': _this.width(),
             'top': opts.margin
         });
         win = $(window)
         var top = _this.offset().top - parseFloat(_this.css('marginTop').replace(/auto/, 0))
 
         var func = function (event) {
+            if (win.width() < opts.minWidth) {
+                _this.css('position', 'static');
+                return;
+            }
             if (win.scrollTop() >= top - opts.margin) {
+                _this.css('width', _this.width());
                 _this.css('position', 'fixed')
             } else {
                 _this.css('position', 'static')
