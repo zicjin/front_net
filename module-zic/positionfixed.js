f9c57c98157383e811f40084b988c1256b86edb3
cheney jin <zicjin@gmail.com>
2014-04-19 23:19:20 +0800

    .

--- module-zic/positionfixed.js
+++ module-zic/positionfixed.js
@@ -6,41 +6,46 @@ define(function (require, exports, module) {
     // nav{position:relative;
     //     &.fixed{position:fixed;top: 0;}
     // }
+    var win = $(window)
     $.fn.positionfixed = function (options) {
         var opts = $.extend({
             margin: 0,
-            lazy: 10, //eye critical point
+            lazy: 16, //eye critical point
             callBack: false,
-            minWidth: 0
+            minWidth: 0,
+            adaptWidth: false
         }, options)
 
-        _this = $(this)
-        _this.css({
-            'top': opts.margin
-        });
-        win = $(window)
-        var top = _this.offset().top - parseFloat(_this.css('marginTop').replace(/auto/, 0))
+        this.each(function () {
+            var _this = $(this)
+            _this.css({
+                'top': opts.margin
+            });
 
-        var func = function (event) {
-            if (win.width() < opts.minWidth) {
-                _this.css('position', 'static');
-                return;
+            var top = _this.offset().top - parseFloat(_this.css('marginTop').replace(/auto/, 0))
+
+            var func = function (event) {
+                if (win.width() < opts.minWidth) {
+                    _this.css('position', 'static');
+                    return;
+                }
+                if (win.scrollTop() >= top - opts.margin) {
+                    if (opts.adaptWidth)
+                        _this.css('width', _this.width())
+                    _this.css('position', 'fixed')
+                } else {
+                    _this.css('position', 'static')
+                }
+                if (opts.callBack) opts.callBack()
             }
-            if (win.scrollTop() >= top - opts.margin) {
-                _this.css('width', _this.width());
-                _this.css('position', 'fixed')
+
+            if (opts.lazy) {
+                var lazyFunc = _.debounce(func, opts.lazy)
+                win.on('scroll', lazyFunc)
             } else {
-                _this.css('position', 'static')
+                win.on('scroll', func)
             }
-            if (opts.callBack) opts.callBack()
-        }
-
-        if (opts.lazy) {
-            var lazyFunc = _.debounce(func, opts.lazy)
-            win.scroll(lazyFunc)
-        } else {
-            win.scroll(func)
-        }
+        });
 
     }
 
